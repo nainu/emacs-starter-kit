9e8b738902218f10e68ff0bf81e8765da163b33c
nainu <nine.cow@gmail.com>
2011-03-06 01:13:34 +0900

    added textmate-mode

--- nainu.el
+++ nainu.el
@@ -6,3 +6,7 @@
 
 (add-to-list 'load-path "~/.emacs.d/nainu/scala-mode")
 (require 'scala-mode-auto)
+
+(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
+(require 'textmate)
+(textmate-mode)
